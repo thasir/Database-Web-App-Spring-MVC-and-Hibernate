be6e4f5c8890532479ea8f476be6f61dfc37ce5e
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-27 14:50:47 +0530

    Sort Customer by LastName

--- web-customer-tracker/src/springdemo/dao/CustomerDAOimpl.java
+++ web-customer-tracker/src/springdemo/dao/CustomerDAOimpl.java
@@ -22,7 +22,7 @@ public class CustomerDAOimpl implements CustomerDAO {
 		// get the current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
 		// create a query
-		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName", Customer.class);
 		// execute query and get the result list
 		List<Customer> customers = theQuery.getResultList();
 		// return the results
