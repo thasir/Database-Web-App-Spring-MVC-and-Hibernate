acbda8dec3ca4d92bd9859bf1f63677aa29323c3
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-27 13:40:29 +0530

    Refactor:Add a Service Layer & @GetMapping and PostMapping

--- web-customer-tracker/src/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/springdemo/controller/CustomerController.java
@@ -5,22 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import springdemo.dao.CustomerDAO;
 import springdemo.entity.Customer;
+import springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	// need to inject the customer dao
+	// need to inject the customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
-		// get customer from the dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		// get customer from the Service
+		List<Customer> theCustomers = customerService.getCustomers();
 		// add customer to the model
 		theModel.addAttribute("customers", theCustomers);
 		return "list-customers";
---
+++ web-customer-tracker/src/springdemo/service/CustomerService.java
@@ -0,0 +1,9 @@
+package springdemo.service;
+
+import java.util.List;
+
+import springdemo.entity.Customer;
+
+public interface CustomerService {
+	public List<Customer> getCustomers();
+}
